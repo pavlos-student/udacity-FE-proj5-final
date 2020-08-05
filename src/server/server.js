a6e161c16bbe1d7e556d02e8fb90a8ce0370004a
Boulos Ananian <pavlos8@gmail.com>
2020-08-05 09:28:49 +0200

    feat: Initial commit

---
+++ src/server/server.js
@@ -0,0 +1,64 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// TODO: prod - uncomment the 'dist' folder
+// Initialize the main project folder
+app.use(express.static('src/client'));
+// app.use(express.static('dist'))
+
+// TODO: prod (change to 8081)
+// Setup Server
+const port = 8080;
+const server = app.listen(port, () => {
+    console.log(`The server is running on port ${port}`)
+});
+
+// TODO: prod
+// app.use(express.static('dist'))
+app.use(express.static('src/client'))
+
+
+/* HTTP Routes - START */
+
+// TODO: prod - uncomment the dist/index.html
+// webpack outputs to the 'dist' folder
+app.get('/', (req, res) => {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// GET
+app.get('/all', (req, res) => {
+    res.send(projectData);
+    console.log('this response was sent: ' + projectData);
+});
+
+// POST
+app.post('/add', (req, res) => {
+    console.log('Coming Request: ' + req.body);
+    let newInfoObj = {
+        date: req.body.date,
+        temp: req.body.temp,
+        content: req.body.content
+    }
+    projectData = newInfoObj;
+    console.log("new info: " + projectData);
+    res.send(projectData);
+});
+/* HTTP Routes - END */
