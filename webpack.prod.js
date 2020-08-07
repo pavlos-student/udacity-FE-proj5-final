b5623ebb5eb2b3e4fa949fd8481759b18512be06
Boulos Ananian <pavlos8@gmail.com>
2020-08-07 07:43:31 +0200

    feat: Added Service Workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin') // to dynamically create the HTML file linked to the main.js
 const { CleanWebpackPlugin } = require('clean-webpack-plugin') // to clean the dist folder automatically before each build
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -44,7 +45,8 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ],
     output: {
         libraryTarget: 'var',
